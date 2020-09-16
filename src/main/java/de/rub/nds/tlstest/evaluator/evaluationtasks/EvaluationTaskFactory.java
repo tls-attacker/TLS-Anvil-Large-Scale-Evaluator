49c78aaf689dd4cb81f85a0e1dd580d48580a22c
Philipp Nieting <philipp@nieting.de>
2020-09-16 18:23:08 +0200

    Added license

--- src/main/java/de/rub/nds/tlstest/evaluator/evaluationtasks/EvaluationTaskFactory.java
+++ src/main/java/de/rub/nds/tlstest/evaluator/evaluationtasks/EvaluationTaskFactory.java
@@ -1,3 +1,12 @@
+/**
+ * TLS-Testsuite-Large-Scale-Evaluator - A tool for executing the TLS-Testsuite against multiple targets running in Docker containers in parallel
+ *
+ * Copyright 2020 Ruhr University Bochum and
+ * TÜV Informationstechnik GmbH
+ *
+ * Licensed under Apache License 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
 package de.rub.nds.tlstest.evaluator.evaluationtasks;
 
 import de.rub.nds.tlstest.evaluator.Config;
